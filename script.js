32f0c63123de83709987920f0d81d03f8e192b6d
negR95 <negar.mahdavi95@gmsil.com>
2022-06-14 18:03:01 +0430

    changes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const startButton = document.getElementById('start');
 const pauseButton = document.getElementById('pause');
 const stopButton = document.getElementById('stop');
+
 const hour = document.getElementById('hour');
 const minute = document.getElementById('min');
 const second = document.getElementById('sec');
